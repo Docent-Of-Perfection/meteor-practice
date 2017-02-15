aafc1ad762402b81794040f3e4c855b6e24325ee
docent <docent@docent.tech>
2017-02-15 05:34:02 -0800

    Added classes, implemented lifecycle methods, refactoring of more button

--- client/components/employee_list.js
+++ client/components/employee_list.js
@@ -1,29 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
 import { Employees } from '../../imports/collections/employees';
 import EmployeeDetail from './employee_detail';
 
 const per_page = 20;
-
-const EmployeeList = (props) => {
+//All classes must return a render method, which in turn must return some amount of JSX
+class EmployeeList extends Component {
   //props.employees => an array of employee objects?
   //Employee detail scaffolindg |  console.log(props.employees);
 
+//see: lifecycle methods
+componentWillMount() {
+  this.page = 1;
+}
 
-console.log(props.employees);
+  handleButtonClick(){
+    Meteor.subscribe('employees', PER_PAGE * (this.page + 1));
+    this.page += 1;
+  }
+render(){
 
 return(
   <div>
   <div className="employee-list">
-      { props.employees.map(employee => <EmployeeDetail key={employee._id} employee={employee} />
+
+      { this.props.employees.map(employee => <EmployeeDetail key={employee._id} employee={employee} />
+        //inside a class based component, you need 'this' before props.employees.map.
+        //this.props initialization occurs in Component constructor
       )}
       </div>
 
-      <button onClick={() => Meteor.subscribe('employees', 40)} className="btn btn-primary"> Load More... </button>
+      <button onClick={
+//Because the below is a callback, the context must be bound
+        this.handleButtonClick.bind(this) } className="btn btn-primary"> Load More... </button>
       </div>
 
 );
-
+}
 };
 
 export default createContainer(() => {
